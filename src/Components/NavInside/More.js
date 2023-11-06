268814dbf615a1bc748a22ac988da10543135588
BasakKamil <Bastric91@gmail.com>
2023-11-06 18:19:46 +0100

    MoRe!

--- src/Components/NavInside/More.js
+++ src/Components/NavInside/More.js
@@ -6,63 +6,6 @@ import { useLayoutEffect } from 'react'
 const More = () => {
 
 
-  // const lat = 52.185970;
-  // const long = 21.184840;
-  // const proxy = 'https://cors-anywhere.herokuapp.com/';
-  // // // const api = `${proxy}https://api.darksky.net/forecast/a18f321825c2c6503931cf827ff61142/${lat},${long}`;
-
-  // const [ikon,setIkon] = useState(
-  //   {
-  //     skycons: new Skycons({color: "white"})
-  //   }
-  
-  // )
-// useEffect(()=>{
-
-// setIkon({})
-
-
-
-// },[]);
- 
-              
-//             
-             
-//           
-//   
-//               const kamilaserver = `${proxy}https://nodeback-js.herokuapp.com/`;
-//              
- 
-//                 fetch(api)
-//                 .then(response => response.json())
-//                 //  .then(data => console.log(data))
-//                  .then(data=>{
-                  
-//                   this.place = data.timezone;
-//                   this.temperature = data.currently.temperature;
-//                   this.new = ((this.temperature - 32) * 5/9);
-//                   // Zaokragle sobie temp do całości
-//                   this.real = Math.round(this.new);
-//                   const warsow = `<p> Obecnie w ${this.place} mamy:</p><h1> ${this.real}<img className="imgC" style="width:30px; margin-left:20px" src=${Temp} /></h1>`;
-//                   document.querySelector('.Tempka').innerHTML = warsow;
-//                   this.icon = data.currently.icon;
-//                   this.currentIcon =  this.icon.replace(/-/g,"_").toUpperCase();
-//                   // ustawiam na stanie icone odpowiednia do pogody obecnej w Wawie 
-//                   this.setState({
-//                     icon: this.currentIcon
-//                   })
-//                   console.log(this.currentIcon);
-//                  });
-        
-//          
-//            
-
-
-
-//     
-        
-    
-//     }
 
 useLayoutEffect(() => {
 
