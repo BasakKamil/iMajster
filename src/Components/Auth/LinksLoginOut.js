30687587f1c21011f4a01c7ff748d425b46e9a06
BasakKamil <Bastric91@gmail.com>
2024-01-07 16:14:12 +0100

    Sent

--- src/Components/Auth/LinksLoginOut.js
+++ src/Components/Auth/LinksLoginOut.js
@@ -3,6 +3,9 @@ import {Link} from 'react-router-dom';
 import Language from '../Layaut/Language';
 import { useTranslation } from 'react-i18next';
 import Burger from '../../images/burger.png'
+import { RiLoginBoxLine } from "react-icons/ri";
+import { VscAccount } from "react-icons/vsc";
+import { BiMailSend } from "react-icons/bi";
 
 function LinksLoginOut(){
 
@@ -16,9 +19,9 @@ function LinksLoginOut(){
             {dropDownOpen ? <div className='Op'>
                <ul>
                  <li><div  onClick={Toggle} className="NowyBut"><span>{t('Menu.login.close')}</span></div></li>
-                 <li><div  className="NowyK"><Link onClick={Toggle} to="/signin">{t('Menu.login.log_in')}</Link></div></li>
-                 <li><div  className="NowyK"><Link onClick={Toggle} to="/signup">{t('Menu.login.register')}</Link></div></li> 
-                 <li><div  className="NowyK"><Link onClick={Toggle} to="/contact">{t('Contact.title')}</Link></div></li> 
+                 <li><div  className="NowyK"><Link onClick={Toggle} to="/signin"><p>{t('Menu.login.log_in')}</p><RiLoginBoxLine /></Link></div></li>
+                 <li><div  className="NowyK"><Link onClick={Toggle} to="/signup"><p>{t('Menu.login.register')}</p><VscAccount /></Link></div></li> 
+                 <li><div  className="NowyK"><Link onClick={Toggle} to="/contact"><p>{t('Contact.title')}</p><BiMailSend /></Link></div></li> 
                  <li><Language/></li>
                </ul>
                <span className='BasiBas'>{t('CRB')}</span> 
